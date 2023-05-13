e34b35ddd37d740d032ca9ad80fbc56967a2b1f2
Roham H <100292931+Rh2001@users.noreply.github.com>
2023-05-13 17:12:33 +0330

    Add square and changing colors
    Update the rendering to also include the ability to draw a rectangle(square) alongside a triangle and to also change the color of our shape as time goes by.

--- OpenTK Triangle/OpenTK Triangle/Program.cs
+++ OpenTK Triangle/OpenTK Triangle/Program.cs
@@ -7,15 +7,23 @@ using OpenTK.Windowing.Common;
 using OpenTK_Triangle;
 class Program
 {
+    public static string? shape;
+    public float[] vertices;
     public static void Main()
     {
-       
+
+        Console.WriteLine("Enter the shape you would like to see: (Triangle or Rectangle)");
+        shape = Console.ReadLine();
+
+        
+
+
         var nativeSettings = new NativeWindowSettings()
         {
-            Title = "Triangle",
+            Title = "Shape",
             Size = new Vector2i(800, 600)
         };
-        
+
         using (var window = new Window(GameWindowSettings.Default, nativeSettings))
         {
             window.Run();
@@ -23,5 +31,26 @@ class Program
 
 
 
+    }
+
+    public static float[] sendVertices(string shape) 
+    {
+        if (shape.ToLower() == "triangle")
+        {
+            return new float[] {
+                    0f , 0.5f, 0f,
+                    0.5f, -0.5f, 0f,
+                   -0.5f,-0.5f, 0f};
+        }
+        else 
+        {
+            return new float[]{
+                0.5f,  0.5f, 0.0f,  // top right
+                0.5f, -0.5f, 0.0f,  // bottom right
+               -0.5f, -0.5f, 0.0f,  // bottom left
+               -0.5f,  0.5f, 0.0f   // top left
+};
+        }
+        
     }
 }
--- OpenTK Triangle/OpenTK Triangle/Window.cs
+++ OpenTK Triangle/OpenTK Triangle/Window.cs
@@ -7,36 +7,64 @@ using OpenTK.Graphics.OpenGL4;
 using OpenTK.Windowing.Common;
 using OpenTK.Windowing.GraphicsLibraryFramework;
 
+
 namespace OpenTK_Triangle
 {
     public class Window : GameWindow
     {
+        float red = 0.0f, green = 1.0f, blue = 1.0f;
+        string shape;
         //Manually write the shader for this program
         string vertexShaderSource = @"
                 #version 400
                 layout(location = 0) in vec3 aPosition;
+               
+                
+                out vec4 vertexColor;
                 void main()
                 {
                     gl_Position = vec4(aPosition, 1.0);
+                    vertexColor = vec4(0.0, 0.0, 0.0, 0.0);
                 }
             ";
 
         string fragmentShaderSource = @"#version 400
+                
                 out vec4 fragColor;
-                void main()
-                {
-                    fragColor = vec4(1, 0.5, 0, 1.0);
-                }
-";
+                uniform vec4 currentColor;
+
+                    void main()
+                        {
+                            fragColor = currentColor;
+                        }
+                    ";
+
+
 
 
 
         //Creates a set of vertices for a triangle
-        private readonly float[] vertices =
-            { 0f , 0.5f, 0f,
-              0.5f, -0.5f, 0f,
-              -0.5f,-0.5f, 0f
-            };
+        //private readonly float[] vertices =
+        //    { 0f , 0.5f, 0f,
+        //      0.5f, -0.5f, 0f,
+        //      -0.5f,-0.5f, 0f
+        //    };
+
+
+        //private float[] vertices;
+
+        private float[] vertices;
+
+        uint[] indices = {  // note that we start from 0!  these are the indices that tells us we need to use two triangles to make a square
+            0, 1, 2,   // first triangle
+            2, 3, 0    // second triangle
+        };
+
+
+ 
+
+
+
         //VBO and VAO are declared here for ease
         private int _vertexBufferObject;
 
@@ -66,37 +94,61 @@ namespace OpenTK_Triangle
             GL.DeleteShader(fragmentShader);
         }
 
+
+        //gets the vertices for both a square and a triangle depending on user input, if the input is null it instead draws a perpendicular triangle using the square vertices
+        private void GetVertices()
+        {
+            if (Program.shape.ToLower() != null)
+                vertices = Program.sendVertices(Program.shape);
+            else
+                vertices = new float[] {
+                    0f , 0.5f, 0f,
+                    0.5f, -0.5f, 0f,
+                   -0.5f,-0.5f, 0f};
+
+        }
+
+        
+
         //override the OnLoad function of the GameWindow class
         protected override void OnLoad()
         {
 
-            //Load the base functoins OnLoad() first
+            // Load the base functions OnLoad() first
             base.OnLoad();
 
             CompileShaders();
 
-            //Set background color
+            GetVertices();
+
+            // Set background color
             GL.ClearColor(0.5f, 0.4f, 0.8f, 1.0f);
 
-            //Generate our VBO
+            // Generate our VBO
             _vertexBufferObject = GL.GenBuffer();
 
-            //Bind our VBO
+            // Bind our VBO
             GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject);
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
 
-            //Upload the data to the GPU buffer
-            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);   //Note: StaticDraw means that our buffer will remain static
-
-
-            //Generate the VAO
+            // Generate the VAO
             _vertexArrayObject = GL.GenVertexArray();
 
-            //Bind it
+            // Bind it
             GL.BindVertexArray(_vertexArrayObject);
 
             GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
             GL.EnableVertexAttribArray(0);
 
+         
+
+
+            if (Program.shape.ToLower() == "square")
+            {
+                int _elementBufferObject = GL.GenBuffer();
+                GL.BindBuffer(BufferTarget.ElementArrayBuffer, _elementBufferObject);
+                GL.BufferData(BufferTarget.ElementArrayBuffer, indices.Length * sizeof(uint), indices, BufferUsageHint.StaticDraw);
+            }
 
 
 
@@ -105,13 +157,38 @@ namespace OpenTK_Triangle
 
         protected override void OnRenderFrame(FrameEventArgs e)
         {
+
+          
             GL.Clear(ClearBufferMask.ColorBufferBit);
+
             GL.UseProgram(_shaderProgram);
+            float timeValue = (float)GLFW.GetTime() / 5.0f;
+            
+            //define RGB colors to change according to time
+            float red = (MathF.Sin(timeValue * 2.0f) + 1.0f) / 2.0f;
+            float green = (MathF.Sin(timeValue * 3.0f) + 1.0f) / 2.0f;
+            float blue = (MathF.Sin(timeValue * 4.0f) + 1.0f) / 2.0f;
+
+            //get the uniform color from our shader
+            int vertexColor = GL.GetUniformLocation(_shaderProgram,"currentColor");
+            GL.Uniform4(vertexColor, red, green, blue, 1.0f);
             GL.BindVertexArray(_vertexArrayObject);
-            GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+
+            //which shape to draw
+            if (Program.shape.ToLower() == "square")
+                GL.DrawElements(PrimitiveType.Triangles, indices.Length, DrawElementsType.UnsignedInt, 0);
+            else
+                GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+
+           
+
+
             SwapBuffers();
             Console.WriteLine("Render Time: {0}", this.RenderTime);
+           
         }
+
+
 
 
         protected override void OnResize(ResizeEventArgs e)
@@ -132,7 +209,7 @@ namespace OpenTK_Triangle
            
         }
 
-        public Window(GameWindowSettings gameSettings, NativeWindowSettings nativeSettings) : base(gameSettings, nativeSettings) { }
+        public Window(GameWindowSettings gameSettings, NativeWindowSettings nativeSettings) : base(gameSettings, nativeSettings) {  }
 
     }
 }
